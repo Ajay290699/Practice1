4976b811baae6e2aae7a2d4d2a8b2607e5ccd341
Ajay Chavan <ajayc9838@gmail.com>
2022-11-01 14:19:25 +0530

    completed star program

--- src/StarPatternProgram.java
+++ src/StarPatternProgram.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class StarPatternProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of row for getting the pattern of star");
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            for (int k = 1; k <= num; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
