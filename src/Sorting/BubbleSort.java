6cb3bf95eea63aa40adca354e229a52f57e0554d
Ajay Chavan <ajayc9838@gmail.com>
2022-11-03 14:58:16 +0530

    program completed

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -4,12 +4,12 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {5, 6, 9, 7, 15, 23, 41, 25, 1, 13};
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < args.length - i; j++) {
-                if (arr[j - 1] > arr[i]) {
-                    int temp = arr[j - 1];
-                    arr[j - 1] = arr[i];
-                    arr[i] = temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
