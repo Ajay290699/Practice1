e550bd28b77007102b8a5d5c01bce24cf7f94664
Ajay Chavan <ajayc9838@gmail.com>
2022-11-03 13:04:59 +0530

    creating bubble sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,4 +1,24 @@
 package Sorting;
 
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 9, 7, 15, 23, 41, 25, 1, 13};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < args.length - i; j++) {
+                if (arr[j - 1] > arr[i]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        printarray(arr);
+    }
+
+    public static void printarray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
