87e2bf75cb9e485599c9def08f9816ae7023e37c
Ajay Chavan <ajayc9838@gmail.com>
2022-11-05 14:57:56 +0530

    creating password program

---
+++ src/Password.java
@@ -0,0 +1,7 @@
+class Password {
+    public static void main(String[] args) {
+        String input = "GeeksforGeeks!@12";
+    }
+
+
+}
--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -4,6 +4,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {5, 6, 9, 7, 15, 23, 41, 25, 1, 13};
 
+        // for printing in ascending order
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -16,6 +17,7 @@ public class BubbleSort {
         printarray(arr);
     }
 
+    //to print array a method
     public static void printarray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
