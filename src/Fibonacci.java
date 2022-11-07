ef0e6bca14870a279f20fe1564e26d1a6363d5c2
Ajay Chavan <ajayc9838@gmail.com>
2022-11-07 10:07:43 +0530

    program completed

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,13 @@
 public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println("Fibonacci Series : ");
+        int n1 = 0, n2 = 1, n3, n = 10;
+        System.out.print(n1 + " " + n2 + " ");
+        for (int i = 2; i < n; i++) {
+            n3 = n2 + n1;
+            System.out.print(n3 + " ");
+            n1 = n2;
+            n2 = n3;
+        }
+    }
 }
